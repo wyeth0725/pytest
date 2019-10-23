7222b1d86c431e362d6e27621a9a31838ef9b394
wyeth0725 <c125054ycomwork.c.oka-pu.ac.jp>
2019-10-23 23:53:40 +0900

    fix

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pytest
 from collections import namedtuple
 
-
+"""
 class Values:
     def __init__(self):
         self.column_a = "sleep"
@@ -27,3 +27,29 @@ def test_value(tasks):
     val = Values()
     for attr in attr_list:
         assert getattr(val, attr) == getattr(tasks, attr)
+"""
+
+def test_mock(mocker):
+    import os
+    assert os.path.join("\\a\\b", "c\\d\\e") == "\\a\\b\\c\\d\\e"
+    with mocker.patch("os.path.join", return_value = "dummy"):
+        assert os.path.join("\\a\\b", "\\c\\d\\e")
+
+
+class TestCls:
+    a = 200
+    
+    def b(self):
+        return 500
+
+def test_mock_object(mocker):
+    assert TestCls.a == 200
+    with mocker.patch.object(TestCls, "a"):
+        TestCls.a = 300
+        assert TestCls.a == 300
+    TestCls.a == 400
+    assert TestCls.a == 300
+    mock = mocker.MagicMock(return_value=1000)
+    with mocker.patch.object(TestCls, "b", mock):
+        assert TestCls.b() == 1000
+        
